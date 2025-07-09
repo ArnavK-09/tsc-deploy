3038ce16657919c7ca4cee2df8bea8b62a2d53d5
Arnav K <arnavkaushal09@gmail.com>
2025-07-09 21:24:14 +0530

    f

--- packages/github-action/src/index.ts
+++ packages/github-action/src/index.ts
@@ -3,7 +3,7 @@ import * as github from '@actions/github';
 import { z } from 'zod';
 import { snapshotProject, getSvgPreviews, getPngPreviews } from './snapshot-project';
 import fs from 'node:fs';
-import { DEPLOY_URL } from '@tscircuit-deploy/shared/constants';
+import { DEPLOY_URL } from './constants';
 import { ulid } from 'ulid';
 
 const InputSchema = z.object({
