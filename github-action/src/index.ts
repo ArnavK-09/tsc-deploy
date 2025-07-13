0e930026f25aed243a66c33efafdf4a891b3971b
Arnav K <arnavkaushal09@gmail.com>
2025-07-13 14:11:02 +0530

    f

--- github-action/src/index.ts
+++ github-action/src/index.ts
@@ -188,6 +188,7 @@ async function run(): Promise<void> {
   } catch (error) {
     const errorMessage =
     error instanceof Error ? error.message : "Unknown error";
+    core.error(error)
     core.setFailed(`☠️ Workflow failed: ${errorMessage}`);
     process.exit(1);
   }
