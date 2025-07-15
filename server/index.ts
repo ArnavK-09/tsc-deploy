30a36604dafb8a06487b0a14ea26d8d0a03a0882
Arnav K <arnavkaushal09@gmail.com>
2025-07-15 12:13:22 +0530

    F

--- server/index.ts
+++ server/index.ts
@@ -35,7 +35,7 @@ app.use("/*", apiAuth);
 app.route("/", apiRouter);
 
 app.notFound((c) => {
-  return c.json({ error: "Not found" }, 407);
+  return c.json({ error: "Not found" }, 404);
 });
 
 app.onError((err, c) => {
