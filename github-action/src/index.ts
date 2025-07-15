7013915a3097d34efa6fa0a499eba7d82c1b1102
Arnav K <arnavkaushal09@gmail.com>
2025-07-15 11:57:00 +0530

    f

--- github-action/src/index.ts
+++ github-action/src/index.ts
@@ -152,7 +152,7 @@ async function run(): Promise<void> {
       create_release:
       context.eventName == "push" && (inputs.create_release || false),
     };
-    core.info("✅ Deployment request prepared");\
+    core.info("✅ Deployment request prepared");
     const response4  = await fetch("https://example.com")
     console.log(await response4.text());
     console.log("deploymentRequest");
