adbd97919cc449628ca6c1b1e5e4dfbc7acac84c
Arnav K <arnavkaushal09@gmail.com>
2025-07-15 12:05:25 +0530

    d

--- github-action/src/index.ts
+++ github-action/src/index.ts
@@ -157,7 +157,7 @@ async function run(): Promise<void> {
     console.log(await response4.text());
     console.log("deploymentRequest");
     const response2 = await fetch(
-      `${inputs.deployServerUrl}/api/process`,
+      `${inputs.deployServerUrl}/process`,
       {
         method: 'POST',
         body: JSON.stringify({}),
